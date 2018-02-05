9409ef4fc89ca6ce83b42df751c4de52a09afb13
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-02-05 12:32:01 -0800

    (refactor) Adjust schema to allow song covers

--- database/schema/songs.sql
+++ database/schema/songs.sql
@@ -1,18 +1,16 @@
 CREATE TABLE IF NOT EXISTS "songs" (
 "id" serial NOT NULL,
-"title" varchar(1024) NOT NULL UNIQUE,
+"title" varchar(1024) NOT NULL,
 "length" int NOT NULL, -- length is in seconds
 "artist" varchar(256) NOT NULL,
-"genre" varchar(256) NOT NULL,
-"popularity" varchar(256), 
+"genre" varchar(256) NOT NULL, 
 "file" varchar(256),
-"playcount" int, -- Somehow set default to 0? Probably account for it later
 "album" varchar(256),
 "track" int,
 "art" varchar(256),
-"releasedate" date, -- YYYY-MM-DD
-"addeddate" date, 
 "bpm" int,
+"popularity" varchar(256),
+"playcount" int, -- Somehow set default to 0? Probably account for it later
 CONSTRAINT songs_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS = FALSE
