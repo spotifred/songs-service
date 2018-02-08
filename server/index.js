a18c6d97c8a0fe66d81c2d9cd8e9368d7daa3ee2
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-02-07 16:09:59 -0800

    (feat) Create working server routes

--- server/index.js
+++ server/index.js
@@ -3,54 +3,60 @@ const Router = require('koa-router');
 const Parser = require('koa-body');
 const logger = require('koa-morgan');
 const responseTime = require('koa-response-time');
-
 const db = require('../database/index.js');
-
-// might not need to have the /index.js here?
 
 const app = new Koa();
 const router = new Router();
 const PORT = 3000; // could do productionPort || 3000 as needed
 
-router.get('/test/:id', (ctx, next) => {
-  // ctx.router available
-  console.log('88888888888888888888888888888888888888888888888', ctx);
-  ctx.body = `test id is ${ctx.params.id}`;
-});
-
 // for immediate relief
-router.get('/songs/immediateDeets/:songID', (ctx, next) => {
-  // ctx.router available
-  // ctx.body = db.getSongDetails(ctx.params.songID);
-  console.log('/songs/immediateDeets/:songID');
+// TODO: refactor to cb for speed
+router.get('/songs/immediateDeets/:songID', async (ctx, next) => {
+  try {
+    ctx.response.body = await db.getSongDetails(ctx.params.songID);
+    await next();
+  } catch (err) {
+    console.error(err);
+    ctx.response.body = err;
+  }
 });
 
-router.get('/songs/deets', (ctx, next) => {
-  // ctx.router available
-  // db.getManyDetails()
+router.get('/songs/deets/:songID', async (ctx, next) => {
+  try {
+    ctx.response.body = await db.getSongDetails(ctx.params.songID);
+    await next();
+  } catch (err) {
+    console.error(err);
+    ctx.response.body = err;
+  }
 });
 
-router.post('/', (ctx, next) => {
-  // ctx.router available
-  // db.addSong()
+// here the body of the post is the song object
+router.post('/songs/addSong', async (ctx, next) => {
+  try {
+    ctx.response.body = await db.addSong(ctx.request.body);
+    await next();
+  } catch (err) {
+    console.error(err);
+    ctx.response.body = err;
+  }
 });
 
-router.post('/songs/addSong', (ctx, next) => {
-  // ctx.router available
-  // db.removeSong()
-  console.log(ctx.req.body);
-  ctx.body = ctx.req.body || 'Hello There';
-});
-
-router.post('/songs/removeSong', (ctx, next) => {
-  // ctx.router available
+router.post('/songs/removeSong/:songID', async (ctx, next) => {
+  try {
+    ctx.response.body = await db.removeSong(ctx.params.songID);
+    await next();
+  } catch (err) {
+    console.error(err);
+    ctx.response.body = err;
+  }
 });
 
 app
   .use(responseTime())
-  .use(logger('combined'))
+  .use(Parser())
   .use(router.routes())
-  .use(router.allowedMethods())
-  .use(Parser());
+  .use(logger('combined'))
+  .use(router.allowedMethods());
 
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
