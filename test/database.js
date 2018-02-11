8999470d67228e74ab46107ec251b634a534423a
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-02-10 17:27:56 -0800

    (test) Write tests for all DB, refactor and add to server tests

--- test/database.js
+++ test/database.js
@@ -4,6 +4,9 @@ const { expect } = require('chai');
 const { Client } = require('pg');
 const db = require('../database/index.js');
 
+let songID = 0;
+let arrayOfSongIDs = [];
+
 describe('Database', () => {
   describe('client.connect', () => {
     it('should connect to the database', (done) => {
@@ -19,4 +22,201 @@ describe('Database', () => {
   });
 
   // add at least 5 so I can query them
+  describe('addSong', () => {
+    it('should add a song to the songs table', (done) => {
+      let songObject = {
+        title: 'Test',
+        length: 10,
+        artist: 'Test',
+        genre: 'Test',
+        file: 'Test',
+        playcount: 0,
+        album: 'Test',
+        track: 10,
+        art: 'Test',
+        bpm: 10,
+      };
+      db.addSong(songObject).then((data) => {
+        expect(data).to.a('object');
+        expect(data.id).to.be.a('number');
+        songID = data.id; // For future tests
+        arrayOfSongIDs.push(songID);
+        expect(data.track).to.be.a('number');
+        expect(data.bpm).to.be.a('number');
+        expect(data.playcount).to.be.a('number');
+        expect(data.title).to.be.a('string');
+        expect(data.artist).to.be.a('string');
+        expect(data.genre).to.be.a('string');
+        expect(data.file).to.be.a('string');
+        expect(data.album).to.be.a('string');
+        expect(data.art).to.be.a('string');
+        expect(data.length).to.equal(10);
+        expect(data.track).to.equal(10);
+        expect(data.bpm).to.equal(10);
+        expect(data.playcount).to.equal(0);
+        expect(data.title).to.equal('Test');
+        expect(data.artist).to.equal('Test');
+        expect(data.genre).to.equal('Test');
+        expect(data.file).to.equal('Test');
+        expect(data.album).to.equal('Test');
+        expect(data.art).to.equal('Test');
+        done();
+      });
+    }).timeout(1000);
+  });
+  describe('getManyDetails', () => {
+    it('should add a song to the songs table', (done) => {
+      let songObject = {
+        title: 'Test',
+        length: 10,
+        artist: 'Test',
+        genre: 'Test',
+        file: 'Test',
+        playcount: 0,
+        album: 'Test',
+        track: 10,
+        art: 'Test',
+        bpm: 10,
+      };
+      db
+        .addSong(songObject)
+        .then((data) => {
+          arrayOfSongIDs.push(data.id);
+        })
+        .catch((err) => console.error(err));
+      db
+        .addSong(songObject)
+        .then((data) => {
+          arrayOfSongIDs.push(data.id);
+        })
+        .then(() => {
+          db
+            .getManyDetails(arrayOfSongIDs)
+            .then((data) => {
+              expect(data[0].id).to.equal(arrayOfSongIDs[0]);
+              expect(data[1].id).to.equal(arrayOfSongIDs[1]);
+              expect(data[2].id).to.equal(arrayOfSongIDs[2]);
+              done();
+            })
+            .catch((err) => {
+              console.error(err);
+              done(err);
+            });
+        })
+        .catch((err) => console.error(err));
+    }).timeout(1000);
+  });
+
+  describe('getManyDetails', () => {
+    it('should return the details of many songs', (done) => {
+      let songObject = {
+        title: 'Test',
+        length: 10,
+        artist: 'Test',
+        genre: 'Test',
+        file: 'Test',
+        playcount: 0,
+        album: 'Test',
+        track: 10,
+        art: 'Test',
+        bpm: 10,
+      };
+      let arrayOfSongIDs = [songID];
+      db
+        .addSong(songObject)
+        .then((data) => {
+          arrayOfSongIDs.push(data.id);
+        })
+        .catch((err) => console.error(err));
+      db
+        .addSong(songObject)
+        .then((data) => {
+          arrayOfSongIDs.push(data.id);
+        })
+        .then(() => {
+          db
+            .getManyDetails(arrayOfSongIDs)
+            .then((data) => {
+              expect(data[0].id).to.equal(arrayOfSongIDs[0]);
+              expect(data[1].id).to.equal(arrayOfSongIDs[1]);
+              expect(data[2].id).to.equal(arrayOfSongIDs[2]);
+              done();
+            })
+            .catch((err) => {
+              console.error(err);
+              done(err);
+            });
+        })
+        .catch((err) => console.error(err));
+    }).timeout(1000);
+  });
+
+  describe('getSongDetails', () => {
+    it('should add a song to the songs table', (done) => {
+      db
+        .getSongDetails(songID)
+        .then((data) => {
+          expect(data.id).to.equal(songID);
+          expect(data.track).to.be.a('number');
+          expect(data.bpm).to.be.a('number');
+          expect(data.playcount).to.be.a('number');
+          expect(data.title).to.be.a('string');
+          expect(data.artist).to.be.a('string');
+          expect(data.genre).to.be.a('string');
+          expect(data.file).to.be.a('string');
+          expect(data.album).to.be.a('string');
+          expect(data.art).to.be.a('string');
+          expect(data.length).to.equal(10);
+          expect(data.track).to.equal(10);
+          expect(data.bpm).to.equal(10);
+          expect(data.playcount).to.equal(0);
+          expect(data.title).to.equal('Test');
+          expect(data.artist).to.equal('Test');
+          expect(data.genre).to.equal('Test');
+          expect(data.file).to.equal('Test');
+          expect(data.album).to.equal('Test');
+          expect(data.art).to.equal('Test');
+          done();
+        })
+        .catch((err) => {
+          console.error(err);
+          done(err);
+        });
+    }).timeout(1000);
+  });
+
+  describe('updatePlaycount', () => {
+    it('should update a playcount in the songs table', (done) => {
+      db
+        .updatePlaycount(songID, 100)
+        .then((data) => {
+          expect(1).to.equal(1);
+          expect(data.message).to.be.a('string');
+          expect(data.message).to.equal('Playcount Updated');
+          expect(data.playcount).to.be.a('number');
+          expect(data.playcount).to.equal(100);
+          done();
+        })
+        .catch((err) => {
+          console.error(err);
+          done(err);
+        });
+    }).timeout(1000);
+  });
+
+  describe('removeSong', () => {
+    it('should remove a song from the songs table', (done) => {
+      db
+        .removeSong(songID)
+        .then((data) => {
+          expect(data).to.be.a('object');
+          expect(data.message).to.equal('Song Deleted');
+          done();
+        })
+        .catch((err) => {
+          console.error(err);
+          done(err);
+        });
+    }).timeout(1000);
+  });
 });
--- test/server.js
+++ test/server.js
@@ -3,12 +3,12 @@ require('newrelic');
 const { expect } = require('chai');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
 const server = require('../server/index.js');
 const PORT = process.env.PORT;
 chai.use(chaiHttp);
 
 let songID = 0;
+
 describe('Server', () => {
   describe('/songs/addSong', () => {
     it('should add a song successfully', (done) => {
@@ -18,16 +18,16 @@ describe('Server', () => {
         .type('application/json')
         .send(
           JSON.stringify({
-            title: 'asdf',
-            length: 120,
-            artist: 'asdf',
-            genre: 'asdf',
-            file: 'asdf',
+            title: 'Test',
+            length: 10,
+            artist: 'Test',
+            genre: 'Test',
+            file: 'Test',
             playcount: 0,
-            album: 'asdf',
-            track: 12,
-            art: 'asdf',
-            bpm: 20,
+            album: 'Test',
+            track: 10,
+            art: 'Test',
+            bpm: 10,
           }),
         )
         .end((err, res) => {
@@ -61,16 +61,16 @@ describe('Server', () => {
           expect(res).to.have.status(200);
           expect(res.body).to.be.a('object');
           expect(res.body.id).to.equal(songID);
-          expect(res.body.length).to.equal(120);
-          expect(res.body.track).to.equal(12);
-          expect(res.body.bpm).to.equal(20);
+          expect(res.body.length).to.equal(10);
+          expect(res.body.track).to.equal(10);
+          expect(res.body.bpm).to.equal(10);
           expect(res.body.playcount).to.equal(0);
-          expect(res.body.title).to.equal('asdf');
-          expect(res.body.artist).to.equal('asdf');
-          expect(res.body.genre).to.equal('asdf');
-          expect(res.body.file).to.equal('asdf');
-          expect(res.body.album).to.equal('asdf');
-          expect(res.body.art).to.equal('asdf');
+          expect(res.body.title).to.equal('Test');
+          expect(res.body.artist).to.equal('Test');
+          expect(res.body.genre).to.equal('Test');
+          expect(res.body.file).to.equal('Test');
+          expect(res.body.album).to.equal('Test');
+          expect(res.body.art).to.equal('Test');
           done();
         });
     }).timeout(1000);
@@ -86,20 +86,49 @@ describe('Server', () => {
           expect(res).to.have.status(200);
           expect(res.body).to.be.a('object');
           expect(res.body.id).to.equal(songID);
-          expect(res.body.length).to.equal(120);
-          expect(res.body.track).to.equal(12);
-          expect(res.body.bpm).to.equal(20);
+          expect(res.body.length).to.equal(10);
+          expect(res.body.track).to.equal(10);
+          expect(res.body.bpm).to.equal(10);
           expect(res.body.playcount).to.equal(0);
-          expect(res.body.title).to.equal('asdf');
-          expect(res.body.artist).to.equal('asdf');
-          expect(res.body.genre).to.equal('asdf');
-          expect(res.body.file).to.equal('asdf');
-          expect(res.body.album).to.equal('asdf');
-          expect(res.body.art).to.equal('asdf');
+          expect(res.body.title).to.equal('Test');
+          expect(res.body.artist).to.equal('Test');
+          expect(res.body.genre).to.equal('Test');
+          expect(res.body.file).to.equal('Test');
+          expect(res.body.album).to.equal('Test');
+          expect(res.body.art).to.equal('Test');
           done();
         });
     }).timeout(1000);
   });
+
+  describe('/songs/getManyDeets', () => {
+    it('should get the details of an array of songs', (done) => {
+      chai
+        .request(`http://localhost:${PORT}`)
+        .post('/songs/getManyDeets')
+        .type('application/json')
+        .send(JSON.stringify([songID]))
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body).to.be.a('array');
+          expect(res.body[0]).to.be.a('object');
+          expect(res).to.have.status(200);
+          expect(res.body[0].id).to.equal(songID);
+          expect(res.body[0].length).to.equal(10);
+          expect(res.body[0].track).to.equal(10);
+          expect(res.body[0].bpm).to.equal(10);
+          expect(res.body[0].playcount).to.equal(0);
+          expect(res.body[0].title).to.equal('Test');
+          expect(res.body[0].artist).to.equal('Test');
+          expect(res.body[0].genre).to.equal('Test');
+          expect(res.body[0].file).to.equal('Test');
+          expect(res.body[0].album).to.equal('Test');
+          expect(res.body[0].art).to.equal('Test');
+          done();
+        });
+    }).timeout(1000);
+  });
+
   describe('/songs/removeSong/:songID', () => {
     it('should add a song successfully', (done) => {
       chai
@@ -116,4 +145,5 @@ describe('Server', () => {
     }).timeout(1000);
   });
 });
+console.log(process.exit);
 after(() => process.exit(0));
